d53a882e66330ecdb828c68753822b2e78cb6f8a
ｃｃｃｃｌｅ <>
2022-09-22 23:14:04 +0800

    ｃｈｏｒｅ：　ｉｎｉｔ

---
+++ backend/src/main/java/com/cle/pojo/doctor.java
@@ -0,0 +1,52 @@
+package com.cle.pojo;
+
+public class doctor {
+    int dictorId;
+    String doctorName;
+    int age;
+    String position;
+    String department;
+
+    public doctor() {
+    }
+
+    public int getDictorId() {
+        return dictorId;
+    }
+
+    public void setDictorId(int dictorId) {
+        this.dictorId = dictorId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
