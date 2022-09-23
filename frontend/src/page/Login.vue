774c11da0065be06de8db58afb3c7db15014a043
凤之兮原 <1577310596@qq.com>
2022-09-23 21:40:14 +0800

    feat: add Login.vue

--- frontend/src/page/Login.vue
+++ frontend/src/page/Login.vue
@@ -1,43 +1,46 @@
 <template>
-  <div w-80 h-50 m-0>
-    <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish" @finishFailed="onFinishFailed">
-      <a-form-item label="Username" name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]">
-        <a-input v-model:value="formState.username">
-          <template #prefix>
-            <UserOutlined class="site-form-item-icon" />
-          </template>
-        </a-input>
-      </a-form-item>
-
-      <a-form-item label="Password" name="password"
-        :rules="[{ required: true, message: 'Please input your password!' }]">
-        <a-input-password v-model:value="formState.password">
-          <template #prefix>
-            <LockOutlined class="site-form-item-icon" />
-          </template>
-        </a-input-password>
-      </a-form-item>
-
-      <a-form-item>
-        <a-form-item name="remember" no-style>
-          <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+  <div w-120 m-auto class="center">
+    <a-card title="Login" :border="false">
+      <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
+        @finishFailed="onFinishFailed">
+        <a-form-item label="Username" name="username"
+          :rules="[{ required: true, message: 'Please input your username!' }]">
+          <a-input v-model:value="formState.username">
+            <template #prefix>
+              <UserOutlined class="site-form-item-icon" />
+            </template>
+          </a-input>
         </a-form-item>
-        <a class="login-form-forgot" href="">Forgot password</a>
-      </a-form-item>
 
-      <a-form-item>
-        <a-button :disabled="disabled" type="primary" html-type="submit" class="login-form-button">
-          Log in
-        </a-button>
-        Or
-        <a href="">register now!</a>
-      </a-form-item>
-    </a-form>
+        <a-form-item label="Password" name="password"
+          :rules="[{ required: true, message: 'Please input your password!' }]">
+          <a-input-password v-model:value="formState.password">
+            <template #prefix>
+              <LockOutlined class="site-form-item-icon" />
+            </template>
+          </a-input-password>
+        </a-form-item>
+
+        <a-form-item>
+          <a-form-item name="remember" no-style>
+            <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+          </a-form-item>
+          <a class="login-form-forgot" href="">Forgot password</a>
+        </a-form-item>
+
+        <a-form-item>
+          <a-button :disabled="disabled" type="primary" html-type="submit" class="login-form-button">
+            Log in
+          </a-button>
+          Or
+          <a href="">register now!</a>
+        </a-form-item>
+      </a-form>
+    </a-card>
   </div>
 </template>
 <script lang="ts" setup>
-import { reactive, computed } from 'vue';
+import { reactive, computed, onBeforeMount } from 'vue';
 import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
 interface FormState {
   username: string;
@@ -48,6 +51,9 @@ interface FormState {
   password: '',
   remember: true,
 });
+onBeforeMount(() => {
+  document.querySelector('body')?.setAttribute('style', 'background-color: skyblue');
+})
 const onFinish = (values: any) => {
   console.log('Success:', values);
 };
@@ -60,7 +66,11 @@ const disabled = computed(() => {
 });
 
 </script>
-<style>
+<style lang="scss" scoped>
+v:deep body {
+  background-color: antiquewhite;
+}
+
 #components-form-demo-normal-login .login-form {
   max-width: 300px;
 }
@@ -71,6 +81,13 @@ const disabled = computed(() => {
 
 #components-form-demo-normal-login .login-form-button {
   width: 100%;
+}
+
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
 
