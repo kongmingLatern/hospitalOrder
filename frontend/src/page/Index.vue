b482b560c5870c021d989004b3b261785375fd3a
凤之兮原 <1577310596@qq.com>
2022-09-25 22:19:32 +0800

    feat: add Carousel.vue

--- frontend/src/page/Index.vue
+++ frontend/src/page/Index.vue
@@ -1,11 +1,13 @@
 <template>
   <Header />
+  <Carousel />
 </template>
 
 <script setup lang='ts'>
 import Header from '@/views/index/Header.vue'
+import Carousel from '@/views/index/Carousel.vue'
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
