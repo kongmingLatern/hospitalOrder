620ab01e89b55694ce92bac6ac549ae4fe8979bf
凤之兮原 <1577310596@qq.com>
2022-09-25 20:17:30 +0800

    feat: add a-spin

--- frontend/src/page/Login.vue
+++ frontend/src/page/Login.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="login-container">
     <div w-120 m-auto class="center">
-      <a-card :border="false">
-        <template #title>
-          <h3>Login</h3>
-        </template>
-        <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
-          @finishFailed="onFinishFailed">
-          <a-form-item label="Username" name="username"
-            :rules="[{ required: true, message: 'Please input your username!' }]">
-            <a-input v-model:value="formState.username">
-            </a-input>
-          </a-form-item>
-
-          <a-form-item label="Password" name="password"
-            :rules="[{ required: true, message: 'Please input your password!' }]">
-            <a-input-password v-model:value="formState.password">
-            </a-input-password>
-          </a-form-item>
-
-          <a-form-item>
-            <a-form-item name="remember" no-style>
-              <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+      <a-spin :spinning="spinning">
+        <a-card :border="false">
+          <template #title>
+            <h3>Login</h3>
+          </template>
+          <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
+            @finishFailed="onFinishFailed">
+            <a-form-item label="Username" name="username"
+              :rules="[{ required: true, message: 'Please input your username!' }]">
+              <a-input v-model:value="formState.username">
+              </a-input>
             </a-form-item>
-            <a href="#" float-right>Forgot password</a>
-          </a-form-item>
 
-          <a-form-item text-center>
-            <a-button :disabled="disabled" type="primary" html-type="submit">
-              Log in
-            </a-button>
-            <router-link to="/regist" class="right">Register now!</router-link>
-          </a-form-item>
-        </a-form>
-      </a-card>
+            <a-form-item label="Password" name="password"
+              :rules="[{ required: true, message: 'Please input your password!' }]">
+              <a-input-password v-model:value="formState.password">
+              </a-input-password>
+            </a-form-item>
+
+            <a-form-item>
+              <a-form-item name="remember" no-style>
+                <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+              </a-form-item>
+              <a href="#" float-right>Forgot password</a>
+            </a-form-item>
+
+            <a-form-item text-center>
+              <a-button :disabled="disabled" type="primary" html-type="submit">
+                Log in
+              </a-button>
+              <router-link to="/regist" class="right">Register now!</router-link>
+            </a-form-item>
+          </a-form>
+        </a-card>
+      </a-spin>
     </div>
   </div>
 </template>
@@ -55,6 +57,7 @@ const onFinish = (values: any) => {
   console.log('Success:', values);
   request.post('/api/user/Login', values, ((res: Record<string, any>) => {
     console.log(res);
+    spinning.value = true
     if (res.data.length === 1) {
       setTimeout(() => {
         router.push('/doctor')
