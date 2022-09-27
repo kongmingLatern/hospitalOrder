eea3431f6de45f5a00aeac24259a5977cc5a30c7
凤之兮原 <1577310596@qq.com>
2022-09-27 23:40:25 +0800

    chore: clear struct

--- frontend/src/views/home/RoomList.vue
+++ frontend/src/views/home/RoomList.vue
@@ -3,11 +3,6 @@
     <header color-green>
       科室管理
     </header>
-    <div mb-5>
-      <a-button class="editable-add-btn" float-right ml-2 @click="handleAdd">查询</a-button>
-      <a-button class="editable-add-btn" float-right ml-2 @click="handleAdd">删除</a-button>
-      <a-button class="editable-add-btn" float-right ml-2 @click="handleAdd">添加</a-button>
-    </div>
     <a-table bordered :data-source="dataSource" :columns="columns">
       <template #bodyCell="{ column, text, record }">
         <template v-if="column.dataIndex === 'name'">
