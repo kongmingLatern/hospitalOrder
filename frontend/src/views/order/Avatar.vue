3c28bd933e08173a4ec900339534cd6e3f85b831
凤之兮原 <1577310596@qq.com>
2022-09-28 23:35:59 +0800

    feat: add Avatar.vue

---
+++ frontend/src/views/order/Avatar.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-avatar :size="size">
+    <template #icon>
+      <slot name="icon"></slot>
+    </template>
+  </a-avatar>
+</template>
+
+<script setup lang='ts'>
+const props = defineProps<{
+  size: string | number
+}>()
+</script>
+
+<style scoped>
+
+</style>
