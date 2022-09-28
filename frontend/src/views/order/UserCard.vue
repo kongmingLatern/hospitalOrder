19b44acb966ac989c7b448f98769d30a0577b69e
凤之兮原 <1577310596@qq.com>
2022-09-28 23:36:41 +0800

    feat: add UserCard.vue

--- frontend/src/views/order/UserCard.vue
+++ frontend/src/views/order/UserCard.vue
@@ -1,5 +1,13 @@
 <template>
-  <a-card :title="userInfo.userName" style="width: 300px" :bordered="false">
+  <a-card style="width: 200px">
+    <template #title>
+      <Avatar size="medium">
+        <template #icon>
+          <UserOutlined />
+        </template>
+      </Avatar>
+      <span class="ml-2">{{userInfo.userName}}</span>
+    </template>
     <p>姓名：{{ userInfo.realName}}</p>
     <p>年齡：{{ userInfo.age}}</p>
     <p>取消預約次數:{{ userInfo.cancelCount}}</p>
@@ -9,11 +17,15 @@
 
 <script setup lang='ts'>
 import type { UserType } from '@/type';
+import { UserOutlined } from '@ant-design/icons-vue';
+import Avatar from './Avatar.vue';
 const props = defineProps<{
   userInfo: UserType;
 }>();
 </script>
 
 <style scoped>
-
+.ml-2 {
+  margin-left: 10px;
+}
 </style>
