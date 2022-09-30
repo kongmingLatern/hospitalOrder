484e74ab00e1d92ce5eb9b3fa28a869e0f2cca15
凤之兮原 <1577310596@qq.com>
2022-09-30 22:05:29 +0800

    feat: finish tabs for roomInfo

--- frontend/src/views/order/Tabs.vue
+++ frontend/src/views/order/Tabs.vue
@@ -1,29 +1,34 @@
 <template>
-  <a-tabs v-model:activeKey="activeKey" type="card">
-    <a-tab-pane v-for="item in tabs" :key="item.key">
+  <a-tabs v-model:activeKey="activeKey" type="card" @tabScroll="callback">
+    <a-tab-pane v-for="item in tabs" :key="item.rid">
       <template #tab>
-        <div @click="goTab(item.key)">{{item.tab}}</div>
+        <div @click="goTab(item.rid)">{{item.rname}}</div>
       </template>
       <!-- 展示内容 -->
-      <router-view class="view" />
+      <router-view />
     </a-tab-pane>
   </a-tabs>
 </template>
 <script lang="ts" setup>
 import router from '@/router';
+import type { TabsProps } from 'ant-design-vue';
 import { ref } from 'vue';
 const props = defineProps<{
   tabs: {
-    tab: string
-    key: string
+    rid: string
+    rname: string
   }[];
 }>();
 
 const activeKey = ref('1')
 
 const goTab = (key: string) => {
-  router.push('/order/roomId' + key)
+  router.push('/order/rid' + key)
 }
+
+const callback: TabsProps['onTabScroll'] = val => {
+  console.log(val);
+};
 </script>
 
 <style lang='scss' scoped>
