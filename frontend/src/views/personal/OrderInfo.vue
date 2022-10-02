47c6f591b223987ca6496e15e68e621946695116
凤之兮原 <1577310596@qq.com>
2022-10-02 21:30:48 +0800

    feat: add selectAll order

--- frontend/src/views/personal/OrderInfo.vue
+++ frontend/src/views/personal/OrderInfo.vue
@@ -1,18 +1,42 @@
 <template>
-  <a-descriptions title="User Info" bordered class="desc-container">
-    <a-descriptions-item label="Product">Cloud Database</a-descriptions-item>
-    <a-descriptions-item label="Billing Mode">Prepaid</a-descriptions-item>
-    <a-descriptions-item label="Automatic Renewal">YES</a-descriptions-item>
-    <a-descriptions-item label="Order time">2018-04-24 18:00:00</a-descriptions-item>
-    <a-descriptions-item label="Usage Time" :span="2">2019-04-24 18:00:00</a-descriptions-item>
-    <a-descriptions-item label="Status" :span="3">
-      <a-badge status="processing" text="Running" />
-    </a-descriptions-item>
-    <a-descriptions-item label="Negotiated Amount">$80.00</a-descriptions-item>
-  </a-descriptions>
+  <a-spin :spinning="spinning">
+    <a-descriptions title="预约信息" bordered class="desc-container" v-for="item in orderList" :key="item.uid">
+      <a-descriptions-item label="预约单号" :span="3">{{item.orderId}}</a-descriptions-item>
+      <a-descriptions-item label="预约时间" :span="3">{{item.orderTime}}</a-descriptions-item>
+      <a-descriptions-item label="预约医生" :span="3">{{item.doctorName}}</a-descriptions-item>
+      <a-descriptions-item label="是否取消预约" :span="3">{{item.isCancel ? '是' : '否'}}</a-descriptions-item>
+      <a-descriptions-item label="是否完成">
+        <a-badge v-if="item.isFinish" status="processing" text="Loading" />
+        <a-badge v-else="item.isFinish" status="processing" text="Finish" />
+      </a-descriptions-item>
+    </a-descriptions>
+  </a-spin>
 </template>
 
 <script lang='ts' setup>
+import { getCurrentInstance, reactive, ref } from 'vue';
+import type { OrderListType } from '../../type';
+
+const orderList = reactive<OrderListType[]>([])
+const spinning = ref<Boolean>(true)
+const instance = getCurrentInstance()
+const request = (instance?.proxy as any).$request!
+// 根据 orderId 查询预约信息
+request.get('api/order/selectByUid', {
+  params: {
+    uid: localStorage.getItem('uid') ?? ''
+  }
+}).then((res: Record<string, any>) => {
+  spinning.value = false
+  const lists = res.data
+  console.log(res);
+  lists.forEach((list: OrderListType) => {
+    orderList.push(list)
+  })
+
+})
+
+
 </script>
 
 <style lang='scss' scoped>
