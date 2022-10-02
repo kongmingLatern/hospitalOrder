fb3eed9674c05c4505d1216c608e4e87c99a4878
凤之兮原 <1577310596@qq.com>
2022-10-02 13:46:45 +0800

    feat: finish OrderList.vue

--- frontend/src/views/home/OrderList.vue
+++ frontend/src/views/home/OrderList.vue
@@ -1,24 +1,9 @@
 <template>
   <a-spin :spinning="spinning">
-    <header color-green>
-      医生管理
-    </header>
     <a-table bordered :data-source="dataSource" :columns="columns">
-      <template #bodyCell="{ column, text, record }">
-        <template v-if="column.dataIndex === 'name'">
-          <div class="editable-cell">
-            <div v-if="editableData[record.key]" class="editable-cell-input-wrapper">
-              <a-input v-model="editableData[record.key].name" @pressEnter="save(record.key)" />
-              <check-outlined class="editable-cell-icon-check" @click="save(record.key)" />
-            </div>
-            <div v-else class="editable-cell-text-wrapper">
-              {{ text || ' ' }}
-              <edit-outlined class="editable-cell-icon" @click="edit(record.key)" />
-            </div>
-          </div>
-        </template>
-        <template v-else-if="column.dataIndex === 'operation'">
-          <a-popconfirm v-if="dataSource.length" title="Sure to delete?" @confirm="onDelete(record.key)">
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.dataIndex === 'operation'">
+          <a-popconfirm v-if="dataSource.length" title="Sure to delete?" @confirm="onDelete(record.orderId)">
             <a>Delete</a>
           </a-popconfirm>
         </template>
@@ -27,35 +12,41 @@
   </a-spin>
 </template>
 <script lang="ts" setup>
-import { computed, getCurrentInstance, onMounted, reactive, ref } from 'vue';
-import { CheckOutlined, EditOutlined } from '@ant-design/icons-vue';
+import { getCurrentInstance, onMounted, reactive, ref } from 'vue';
 import { cloneDeep } from 'lodash-es';
 import type { OrderListType } from '@/type';
+import { message } from 'ant-design-vue';
+import { formatData, formatObject } from '../../utils';
+import router from '@/router';
 
 
 const columns = [
   {
-    title: 'orderId',
+    title: '订单号',
     dataIndex: 'orderId',
   },
   {
-    title: 'uid',
-    dataIndex: 'uid',
+    title: '真实姓名',
+    dataIndex: 'realName',
   },
   {
-    title: 'orderTime',
+    title: '预约医生',
+    dataIndex: 'doctorName',
+  },
+  {
+    title: '预约时间',
     dataIndex: 'orderTime',
   },
   {
-    title: 'orderRoom',
-    dataIndex: 'orderRoom',
+    title: '预约科室',
+    dataIndex: 'rname',
   },
   {
-    title: 'isCancel',
+    title: '是否取消',
     dataIndex: 'isCancel',
   },
   {
-    title: 'isFinish',
+    title: '是否完成',
     dataIndex: 'isFinish',
   },
   {
@@ -65,56 +56,35 @@ const columns = [
 ];
 const spinning = ref<Boolean>(true)
 let dataSource: OrderListType[] = reactive([]);
+const instance = getCurrentInstance()
+const request = (instance?.proxy as any).$request!
 onMounted(() => {
-  const instance = getCurrentInstance()
-  const request = (instance?.proxy as any).$request!
-
   request.get('api/order/selectAll').then((res: Record<string, any>) => {
     console.log(res);
     spinning.value = false
     const lists = res.data
     lists.forEach((list: OrderListType) => {
-      dataSource.push(list)
+      dataSource.push(formatObject(list) as OrderListType)
     })
   }).catch((e: any) => {
     console.log(e.message);
   })
-
-  // request.get('api/doctor/selectAll').then((res: Record<string, any>) => {
-  //   // dataSource.push(res.data[0])
-  //   const lists = res.data
-  //   lists.forEach((list: OrderListType) => {
-  //     dataSource.push(list)
-  //   })
-  // }).catch((e: any) => {
-  //   console.log(e.message);
-  // })
-  // console.log(dataSource);
-
 })
-const count = computed(() => dataSource.length + 1);
-const editableData: Record<string, OrderListType> = reactive({});
 
-const edit = (id: string) => {
-  editableData[id] = cloneDeep(dataSource.filter(item => id === item.id)[0]);
-};
-const save = (id: string) => {
-  Object.assign(dataSource.filter(item => id === item.id)[0], editableData[id]);
-  delete editableData[id];
-};
-
-const onDelete = (id: string) => {
-  dataSource = dataSource.filter(item => item.id !== id);
-};
-const handleAdd = () => {
-  const newData = {
-    id: `${count}`,
-    name: `Edward King ${count}`,
-    age: 32,
-    order: '皮肤科',
-    dateTime: new Date().toLocaleDateString()
-  };
-  dataSource.push(newData);
+const onDelete = (orderId: string) => {
+  request.get('/api/order/delete', {
+    params: {
+      orderId
+    }
+  }).then((res: Record<string, any>) => {
+    dataSource = dataSource.filter(item => item.orderId !== orderId);
+    message.success('删除成功')
+    setTimeout(() => {
+      router.go(0)
+    }, 0)
+  }).catch((err: string) => {
+    message.error(err)
+  })
 };
 </script>
 <style lang="scss" scoped>
