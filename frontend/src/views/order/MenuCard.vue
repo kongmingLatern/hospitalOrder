8da5bd17f50621866d3afdb0df2ae49c91da7001
凤之兮原 <1577310596@qq.com>
2022-10-03 22:02:57 +0800

    feat: add a button

--- frontend/src/views/order/MenuCard.vue
+++ frontend/src/views/order/MenuCard.vue
@@ -1,13 +1,19 @@
 <template>
   <a-card title="快捷菜单" style="width: 200px">
-    <router-link to="/personal">
-      <a-button type="primary">个人中心</a-button>
-    </router-link>
+    <div>
+      <a-button type="primary" @click="$router.push('/personal')">个人中心</a-button>
+    </div>
+    <div mt-5>
+      <a-button type="primary" @click="$router.push('/login')">退出登录</a-button>
+    </div>
+    <div mt-5 v-if="isAuth">
+      <a-button type="danger" @click="$router.push('/doctor')">后台管理</a-button>
+    </div>
   </a-card>
 </template>
 
 <script setup lang='ts'>
-
+const isAuth = localStorage.getItem('isAuth')
 </script>
 
 <style scoped>
