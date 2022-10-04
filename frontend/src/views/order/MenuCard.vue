7181dc6faca38da3c93b9e6e99c5f73e3c59f1b6
凤之兮原 <1577310596@qq.com>
2022-10-04 15:29:43 +0800

    feat: finish exitLogin Function

--- frontend/src/views/order/MenuCard.vue
+++ frontend/src/views/order/MenuCard.vue
@@ -4,7 +4,7 @@
       <a-button type="primary" @click="$router.push('/personal')">个人中心</a-button>
     </div>
     <div mt-5>
-      <a-button type="primary" @click="$router.push('/login')">退出登录</a-button>
+      <a-button type="primary" @click="exitLogin">退出登录</a-button>
     </div>
     <div mt-5 v-if="isAuth === 'true'">
       <a-button type="danger" @click="$router.push('/doctor')">后台管理</a-button>
@@ -13,7 +13,13 @@
 </template>
 
 <script setup lang='ts'>
+import router from '@/router';
+
 const isAuth = localStorage.getItem('isAuth')
+const exitLogin = () => {
+  localStorage.clear()
+  router.push('/login')
+}
 </script>
 
 <style scoped>
