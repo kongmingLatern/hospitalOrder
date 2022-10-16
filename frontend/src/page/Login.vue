3cc76f0033f5110208af882f7bcbe205f4b25e9a
凤之兮原 <1577310596@qq.com>
2022-10-16 15:07:37 +0800

    extract: login

--- frontend/src/page/Login.vue
+++ frontend/src/page/Login.vue
@@ -6,30 +6,37 @@
           <template #title>
             <h3>登录</h3>
           </template>
-          <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
-            @finishFailed="onFinishFailed">
-            <a-form-item label="用户名" name="username"
-              :rules="[{ required: true, message: 'Please input your username!' }]">
-              <a-input v-model:value="formState.username">
-              </a-input>
+          <a-form
+            :model="formState"
+            name="normal_login"
+            class="login-form"
+            @finish="onFinish"
+            @finishFailed="onFinishFailed"
+          >
+            <a-form-item
+              label="用户名"
+              name="username"
+              :rules="[{ required: true, message: 'Please input your username!' }]"
+            >
+              <a-input v-model:value="formState.username"></a-input>
             </a-form-item>
 
-            <a-form-item label="密码" name="password"
-              :rules="[{ required: true, message: 'Please input your password!' }]">
-              <a-input-password v-model:value="formState.password">
-              </a-input-password>
-            </a-form-item>
-
-            <a-form-item>
-              <a-form-item name="remember" no-style>
-                <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
-              </a-form-item>
-              <a href="#" float-right>忘记密码</a>
+            <a-form-item
+              label="密码"
+              name="password"
+              :rules="[{ required: true, message: 'Please input your password!' }]"
+            >
+              <a-input-password v-model:value="formState.password"></a-input-password>
             </a-form-item>
 
             <a-form-item text-center>
-              <a-button :disabled="disabled" :loading="loading" type="primary" html-type="submit"
-                data-test="loginButton">
+              <a-button
+                :disabled="disabled"
+                :loading="loading"
+                type="primary"
+                html-type="submit"
+                data-test="loginButton"
+              >
                 登录
               </a-button>
               <router-link to="/regist" class="right">注册</router-link>
@@ -41,15 +48,14 @@
   </div>
 </template>
 <script lang="ts" setup>
-import type { LoginType } from '@/type';
-import { message } from 'ant-design-vue';
-import { reactive, computed, ref, getCurrentInstance } from 'vue';
-import router from '../router';
+import type { LoginType } from '@/type'
+import { message } from 'ant-design-vue'
+import { reactive, computed, ref, getCurrentInstance, toRaw } from 'vue'
+import router from '../router'
 const formState = reactive<LoginType>({
   username: '',
   password: '',
-  remember: true,
-});
+})
 
 const spinning = ref<Boolean>(false)
 const loading = ref<Boolean>(false)
@@ -57,49 +63,54 @@ const instance = getCurrentInstance()
 const request = (instance?.proxy as any).$request!
 
 const onFinish = (values: any) => {
-  const { username, password, remember } = formState
-  request.get('/api/user/Login', {
-    params: {
-      username,
+  const { username: userName, password } = formState
+  console.log(toRaw(formState))
+  request
+    .post('/users/login', {
+      userName,
       password,
-      remember
-    }
-  }).then((res: any) => {
-    if (res.status === 200) {
-      // 展示 Loading 信息
-      loading.value = true
-      const { uid, isAuth, userName } = res.data
-      // 存入缓存信息
-      localStorage.setItem('uid', uid)
-      localStorage.setItem('isAuth', isAuth)
-      localStorage.setItem('username', userName)
+    })
+    .then((res: any) => {
+      console.log(res)
+      // 登录成功
+      if (res.data.code === 20021) {
+        // 展示 Loading 信息
+        loading.value = true
+        const { uid, isAuth, userName } = res.data.data
+        // 存入缓存信息
+        localStorage.setItem('uid', uid)
+        localStorage.setItem('isAuth', isAuth)
+        localStorage.setItem('username', userName)
 
-      setTimeout(() => {
-        if (isAuth) {
-          message.success('登录成功，欢迎您管理员，即将跳转到后台管理');
-          router.push('/doctor')
-        } else {
-          message.success('登录成功');
-          router.push('/ordermanager');
-        }
-      }, 2000)
-    }
-  }).catch((err: Record<string, any>) => {
-    const { status } = err.response
-    if (status === 400) {
-      console.log(err.response);
-      const { message: msg } = err.response.data
-      message.error(msg);
-    }
-  })
-};
+        setTimeout(() => {
+          if (isAuth) {
+            message.success('登录成功，欢迎您管理员，即将跳转到后台管理')
+            router.push('/doctor')
+          } else {
+            message.success('登录成功')
+            router.push('/ordermanager')
+          }
+        }, 2000)
+      } else if (res.data.code === 20000) {
+        message.error('用户名或密码错误')
+      }
+    })
+    .catch((err: Record<string, any>) => {
+      const { status } = err.response
+      if (status === 400) {
+        console.log(err.response)
+        const { message: msg } = err.response.data
+        message.error(msg)
+      }
+    })
+}
 
 const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
-};
+  console.log('Failed:', errorInfo)
+}
 const disabled = computed(() => {
-  return !(formState.username && formState.password) || loading.value;
-});
+  return !(formState.username && formState.password) || loading.value
+})
 </script>
 <style lang="scss" scoped>
 .login-container {
@@ -148,4 +159,3 @@ const disabled = computed(() => {
   position: relative;
 }
 </style>
-
