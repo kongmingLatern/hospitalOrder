0c7f9b63d62d702bc1ce1502649eafb35f7e3c6d
凤之兮原 <1577310596@qq.com>
2022-10-16 15:37:33 +0800

    finish: extract RoomForm

--- frontend/src/components/home/RoomForm.vue
+++ frontend/src/components/home/RoomForm.vue
@@ -2,8 +2,17 @@
   <div>
     <a-button type="primary" @click="visible = true">添加科室</a-button>
     <a-modal v-model:visible="visible" title="Add" ok-text="Create" cancel-text="Cancel" @ok="onOk">
-      <a-form ref="formRef" :model="formState" v-bind="layout" userName="nest-messages" @finish="onFinish" flex
-        flex-wrap flex-col content-start>
+      <a-form
+        ref="formRef"
+        :model="formState"
+        v-bind="layout"
+        userName="nest-messages"
+        @finish="onFinish"
+        flex
+        flex-wrap
+        flex-col
+        content-start
+      >
         <a-form-item name="rname" label="rname">
           <a-input v-model:value="formState.rname" />
         </a-form-item>
@@ -12,52 +21,56 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { getCurrentInstance, reactive, ref, toRaw } from 'vue';
-import { message, type FormInstance } from 'ant-design-vue';
-import { randomString } from '../../utils';
-import type { RoomType } from '../../type';
+import { getCurrentInstance, reactive, ref, toRaw } from 'vue'
+import { message, type FormInstance } from 'ant-design-vue'
+import { randomString } from '../../utils'
+import type { RoomType } from '../../type'
 
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
-};
+}
 
-const formRef = ref<FormInstance>();
-const visible = ref<Boolean>(false);
+const formRef = ref<FormInstance>()
+const visible = ref<Boolean>(false)
 const instance = getCurrentInstance()
 const request = (instance?.proxy as any).$request!
 
 let formState: RoomType = reactive({
   rid: randomString(),
-  rname: ''
-});
+  rname: '',
+})
 
 const emit = defineEmits<{
   (msg: string, formState: RoomType): void
 }>()
 
 const onOk = () => {
-  (formRef as any)?.value
+  ;(formRef as any)?.value
     .validateFields()
     .then((values: any) => {
-      request.post('/api/room/add', toRaw(formState)).then((res: any) => {
-        emit('addRoom', toRaw(formState));
-        message.success(res.data.message)
-      }).catch((err: any) => {
-        message.error(err.data.message)
-      })
-      visible.value = false;
+      request
+        .post('/rooms', toRaw(formState))
+        .then((res: any) => {
+          if (res.data.code === 20011) {
+            emit('addRoom', toRaw(formState))
+            message.success('添加成功')
+          }
+        })
+        .catch((err: any) => {
+          message.error(err.data.message)
+        })
+      visible.value = false
     })
     .catch((info: string) => {
-      console.log('Validate Failed:', info);
-    });
-};
-
-const onFinish = (e: MouseEvent) => {
-  console.log(e);
-  console.log('onFinish');
+      console.log('Validate Failed:', info)
+    })
 }
 
+const onFinish = (e: MouseEvent) => {
+  console.log(e)
+  console.log('onFinish')
+}
 </script>
 <style>
 .collection-create-form_last-form-item {
