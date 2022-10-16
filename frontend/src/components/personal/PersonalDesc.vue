06f3a114417e36e0750ba54eef9467e906dd4b7a
凤之兮原 <1577310596@qq.com>
2022-10-16 16:04:55 +0800

    finish: PersonalDesc API

--- frontend/src/components/personal/PersonalDesc.vue
+++ frontend/src/components/personal/PersonalDesc.vue
@@ -1,20 +1,20 @@
 <template>
-  <a-descriptions title="基本信息" class="desc-container" :column=1 bordered>
-    <a-descriptions-item label="用户名">{{ result.userName}}</a-descriptions-item>
-    <a-descriptions-item label="年龄">{{result.age}}</a-descriptions-item>
-    <a-descriptions-item label="真实姓名">{{result.realName}}</a-descriptions-item>
-    <a-descriptions-item label="取消预约次数">{{ result.cancelCount}}</a-descriptions-item>
-    <a-descriptions-item label="账号是否禁用">{{!result.isAllow ? '否': '是'}}</a-descriptions-item>
-    <a-descriptions-item label="是否是管理员" v-if="result.isAuth">
-      管理员
+  <a-descriptions title="基本信息" class="desc-container" :column="1" bordered>
+    <a-descriptions-item label="用户名">{{ result.userName }}</a-descriptions-item>
+    <a-descriptions-item label="年龄">{{ result.age }}</a-descriptions-item>
+    <a-descriptions-item label="真实姓名">{{ result.realName }}</a-descriptions-item>
+    <a-descriptions-item label="取消预约次数">{{ result.cancelCount }}</a-descriptions-item>
+    <a-descriptions-item label="账号是否禁用">
+      {{ !result.isAllow ? '否' : '是' }}
     </a-descriptions-item>
+    <a-descriptions-item label="是否是管理员" v-if="result.isAuth">管理员</a-descriptions-item>
   </a-descriptions>
 </template>
 
-<script lang='ts' setup>
-import type { UserType } from '@/type';
-import { getCurrentInstance, onMounted, reactive } from 'vue';
-import { hasOwnProperty } from '../../utils';
+<script lang="ts" setup>
+import type { UserType } from '@/type'
+import { getCurrentInstance, onMounted, reactive } from 'vue'
+import { hasOwnProperty } from '../../utils'
 const uid = localStorage.getItem('uid')
 const result = reactive<UserType>({
   uid: '',
@@ -25,32 +25,28 @@ const result = reactive<UserType>({
   cancelCount: 0,
   isAllow: 0,
   isAuth: 0,
-});
+})
 const instance = getCurrentInstance()
 const request = (instance?.proxy as any).$request!
 onMounted(() => {
-  request.get('api/user/selectByUid', {
-    params: {
-      uid
-    }
-  }).then((res: Record<string, any>) => {
-    let key: string
-    for (key in res.data) {
-      if (hasOwnProperty(result, key)) {
-        result[key] = res.data[key]
+  request
+    .get('/users/getByUid/' + uid)
+    .then((res: Record<string, any>) => {
+      console.log(res)
+      let key: string
+      for (key in res.data.data) {
+        if (hasOwnProperty(result, key)) {
+          result[key] = res.data.data[key]
+        }
       }
-    }
-  }).catch((e: any) => {
-    console.log(e.message);
-  })
+    })
+    .catch((e: any) => {
+      console.log(e.message)
+    })
 })
-
-
-
 </script>
 
-
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .desc-container {
   width: 500px;
   margin: 0 auto;
